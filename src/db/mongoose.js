5b15bda54fface38047456ba1c0a535b6a0aee70
catan271 <catan271@gmail.com>
2021-08-12 20:43:44 +0700

    env vars fix

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-mongoose.connect(process.env.mongodb, {
+mongoose.connect(process.env.MONGODB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
